e271d718dbbe2a0968789c9cf69c4aad1c21ea36
tan nguyen <tannpv@gmail.com>
2021-05-23 22:07:54 +0700

    initializing source control

---
+++ app/application.go
@@ -0,0 +1,17 @@
+package app
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/tannpv/bookstore_utils-go/logger"
+)
+
+var (
+	router = gin.Default()
+)
+
+func StartApplication() {
+	mapUrls()
+
+	logger.Info("about to start the application...")
+	router.Run(":8082")
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/southern-martin/user-api/app"
+)
+
+func main() {
+	app.StartApplication()
+}
