143c6ba838196259829260ff9490e96f6de5a42f
tan nguyen <tannpv@gmail.com>
2021-06-02 23:26:31 +0700

    update go module config

---
+++ app/application.go
@@ -0,0 +1,17 @@
+package app
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/southern-martin/util-go/logger"
+)
+
+var (
+	router = gin.Default()
+)
+
+func StartApplication() {
+	mapUrls()
+
+	logger.Info("about to start the application...")
+	router.Run(":8082")
+}
